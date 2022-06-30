2776772a0dabebd8f2a3ba6336b791d3e87e8afb
abenezerdereje <abenidtg@gmail.com>
2022-06-30 15:42:32 -0700

    betty for 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-/* This program uses puts function */
+
+/**
+ * main -Entry point
+ *
+ * uses puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

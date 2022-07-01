09d17c3a8f1c05f15ab67894cf132b9bb9c1b19b
abenezerdereje <abenidtg@gmail.com>
2022-07-01 09:19:38 -0700

    Added 3-

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	a = 65;
+	while (a < 91)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+

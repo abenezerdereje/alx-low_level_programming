c6b5b13f5fcc1caab401298cb302aa7d87113b24
abenezerdereje <abenidtg@gmail.com>
2022-07-06 13:04:43 -0700

    Added 8-

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * jack_bauer- counts every minute of the day
+ */
+void jack_bauer(void)
+{
+	int a = 0;
+
+	while (a < 24)
+	{
+		int b = 0;
+
+		while (b < 60)
+		{
+			char c = 48 + (a / 10);
+			char d = 48 + (a % 10);
+			char e = 48 + (b / 10);
+			char f = 48 + (b % 10);
+
+			_putchar(c);
+			_putchar(d);
+			_putchar(':');
+			_putchar('
+			b++;
+		}
+		a++;
+	}
+}
+
+
+

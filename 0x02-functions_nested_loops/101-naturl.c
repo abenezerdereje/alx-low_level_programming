ed53dceab3bded4492ba8094b168d132dfb43458
abenezerdereje <abenidtg@gmail.com>
2022-07-06 15:04:41 -0700

    Added 101-

---
+++ 0x02-functions_nested_loops/101-naturl.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += a;
+		}
+		a++;
+	}
+	printf("%d\n", sum);
+}

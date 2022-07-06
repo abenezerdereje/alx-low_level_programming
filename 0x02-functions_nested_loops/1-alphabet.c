dfd670aa5ef529c091d87ab4f03c04caa57f1e85
abenezerdereje <abenidtg@gmail.com>
2022-07-06 10:45:27 -0700

    Added 1-

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_alphabet- prints alphabet
+ *
+ * Return: 0
+ */
+int print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a >= 'z')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}

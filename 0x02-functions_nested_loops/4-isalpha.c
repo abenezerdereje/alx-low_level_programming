afb06f2b8f14dd2c033391a49d301ec6c72625c7
abenezerdereje <abenidtg@gmail.com>
2022-07-06 11:37:08 -0700

    Added 4-

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isalpha- checks for alphabetic character
+ * @c: character to be checked
+ *
+ * Return: 1 if alphabet. 0 otherwise
+ */
+int _silower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c<= 122)
+		return (1);
+	else 
+		return (0);
+}

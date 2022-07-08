65ad7cf1bff28438d161d032551367e11d6f5d64
abenezerdereje <abenidtg@gmail.com>
2022-07-07 18:50:37 -0700

    Edited 4-

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,10 @@ void print_most_numbers(void)
 
 	while (i < 10)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(48 + i);
+		if (i != 2 && i != 4)
+		{
+			_putchar(48 + i);
+		}
 		i++;
 	}
 	_putchar(10);

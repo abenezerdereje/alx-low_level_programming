edd9682ac217cfcb93fcfd62e3c2a3fb75109f31
abenezerdereje <abenidtg@gmail.com>
2022-07-07 18:59:44 -0700

    Added 5-

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * more_numbers- prints from 0 to 14, 10 times
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+				_putchar(48 + j / 10);
+			_putchar(48 + j % 10);
+
+		}
+		_putchar(10);
+	}

06ee9a545f7889c2ce0fc9c09b23d539958d0296
abenezerdereje <abenidtg@gmail.com>
2022-07-11 09:11:52 -0700

    Adde 2-

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * _strlen- calculates length of a string
+ * @s: pointer of string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int ctr = 0;
+
+	if (s)
+	{
+		while (*(s + i) != '\0')
+		{
+			ctr++;
+		}
+	}
+
+	return (ctr);
+}

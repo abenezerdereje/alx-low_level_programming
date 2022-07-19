e3c737df22a6bde4386eaaaf33dcc0938d5a8ef8
abenezerdereje <abenidtg@gmail.com>
2022-07-19 10:01:57 -0700

    Edited 2-

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
 
-	while (s[i] != '\0')
+	while (*s++)
 	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (!*s)
+		{
+			return (NULL);
+		}
 	}
 
 	return (NULL);
